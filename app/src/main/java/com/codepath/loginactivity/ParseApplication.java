f1dd06b8b82acb0d59bba894825baa04448ea7f7
AShariff0786 <shariffasim@gmail.com>
2019-03-24 19:31:52 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/loginactivity/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.codepath.loginactivity;
+
+import android.app.Application;
+
+import com.codepath.loginactivity.Post;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("codepath-ig") // should correspond to APP_ID env variable
+                .clientKey("CodepathIGKey")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://codepath-ig.herokuapp.com/parse").build());
+    }
+}
